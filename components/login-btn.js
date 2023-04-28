4779ff6bfe12295fcff4db819add8ea96437f9ad
Marat Miribyan <122740363+Miribyan@users.noreply.github.com>
2023-04-28 21:05:05 +0400

    prisma must be work

--- components/login-btn.js
+++ components/login-btn.js
@@ -1,5 +1,5 @@
 import { useSession, signIn, signOut } from "next-auth/react";
-
+import { useState } from "react";
 export default function Login_Component() {
   const { data: session } = useSession();
   const [message, setMessage] = useState("");
