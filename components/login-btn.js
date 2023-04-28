a7fbc8043f18385351ceff67924eb510ea45d7f4
Marat Miribyan <122740363+Miribyan@users.noreply.github.com>
2023-04-28 21:18:47 +0400

    prisma must be work

--- components/login-btn.js
+++ components/login-btn.js
@@ -1,24 +1,27 @@
 import { useSession, signIn, signOut } from "next-auth/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
 export default function Login_Component() {
   const { data: session } = useSession();
   const [message, setMessage] = useState("");
-
+  useEffect(async () => {
+    const response = await fetch("/api/prisma/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ session }),
+    });
+    const data = await response.json();
+    setMessage(data.message);
+    console.log(data.message);
+  }),
+    [session];
   if (session) {
-    async () => {
-      const response = await fetch("/api/prisma/user", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ session }),
-      });
-      const data = await response.json();
-      setMessage(data.message);
-    };
     return (
       <>
         Signed in as {session.user.name} <br />
+        <p>{message}</p>
         <button onClick={() => signOut()}>Sign out</button>
       </>
     );
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import Login_Component from "../../components/login-btn";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>Posts List</p>
