d7d7562009f2e2a8bc64e340531602e907e5d334
Marat Miribyan <122740363+Miribyan@users.noreply.github.com>
2023-04-28 18:32:41 +0400

    add function for create user when authorised

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Login_Component from "../../components/login-btn";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <p>Posts List</p>
+      <Login_Component />
     </main>
   );
 }
