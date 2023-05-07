b51357a0eb42ba2c2bb3c3b68f881855fba6af8b
Marat Miribyan <122740363+Miribyan@users.noreply.github.com>
2023-05-07 23:15:25 +0400

    header mobile menu update

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -195,7 +195,7 @@ function Header() {
               />
 
               <Link
-                href="/main"
+                href="/"
                 className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
               >
                 {t("common:main")}
