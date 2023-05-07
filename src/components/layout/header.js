a8a2111511ab17263ef2aa4b7c0d615a5f86e9fa
Marat Miribyan <122740363+Miribyan@users.noreply.github.com>
2023-05-07 23:10:33 +0400

    header mobile menu update

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -180,8 +180,8 @@ function Header() {
                     value={currentLang}
                     className="mt-2 w-32 block rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   >
-                    <option>English</option>
-                    <option>Russian</option>
+                    <option value="en">English</option>
+                    <option value="ru">Russian</option>
                   </select>
                 </div>
               </div>
